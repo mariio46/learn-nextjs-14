13ffb0c938a99f73e4039d4c56c6f4a008439bdc
Mario <mariomad2296@gmail.com>
2023-11-22 21:59:47 +0800

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import '@/app/ui/global.css';
+import { figtree } from './ui/fonts';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang='en'>
+            <body className={`${figtree.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Figtree, Lusitana } from 'next/font/google';
+
+export const figtree = Figtree({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700', '400', '700'], subsets: ['latin'], style: ['normal'] });
